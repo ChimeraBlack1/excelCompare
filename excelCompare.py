b1dfe56760ea3f7276b41f0c65884bd2d465ef25
ChimeraBlack1 <christopherparke@zoho.com>
2020-01-28 11:11:13 -0500

    added new methods for saving, newwb, newsheet, etc

--- excelCompare.py
+++ excelCompare.py
@@ -2,24 +2,25 @@ import math
 import xlrd
 import xlwt
 
-def OpenSheet(wb):
+def OpenSheet(wb, tab=0):
   """
   opens a workbook and returns the first sheet
   """
   workbook = xlrd.open_workbook(wb)
-  sheet = workbook.sheet_by_index(0)
-  return sheet 
+  sheet = workbook.sheet_by_index(tab)
+  return sheet
 
-def GetValue(sheet, row, col):
+def GetValue(sheet, row=0, col=0):
+  """
+  Get the value from the cell in a specified sheet
+  """
   serial = sheet.cell_value(row,col)
   return serial
 
-def FindLastPopulatedRow(wb, row=0, col=0):
+def FindLastRow(sheet, row=0, col=0):
   """
   Find the number of populated rows in an excel workbook
   """  
-  workbook = xlrd.open_workbook(wb)
-  sheet = workbook.sheet_by_index(0)
   content = sheet.cell_value(row, col)
   rowCount = 0
 
@@ -27,10 +28,68 @@ def FindLastPopulatedRow(wb, row=0, col=0):
     try:
       content = sheet.cell_value(row + rowCount, col)
     except:
-      rowCount = rowCount +1
-      return rowCount
+      break
     rowCount = rowCount + 1
+    
   return rowCount
+
+
+def FindLastRowZeroIndex(sheet, row=0, col=0):
+  """
+  Find last populated row from starting point, returns 0 index value
+  """
+  content = sheet.cell_value(row, col)
+  rowCount = 0
+
+  while content != "":
+    try:
+      content = sheet.cell_value(row + rowCount, col)
+    except:
+      break
+    rowCount = rowCount + 1
+  
+  #zero indexing
+  rowCount = rowCount - 1
+  if rowCount < 0:
+    rowCount = 0
+
+  return rowCount
+
+def FindLastCol(sheet, row=0, col=0):
+  """
+  Find last populated column from starting point
+  """
+  content = sheet.cell_value(row, col)
+  colCount = 0
+
+  while content != "":
+    try:
+      content = sheet.cell_value(row, col + colCount)
+    except:
+      break
+    colCount = colCount + 1
+  return colCount
+
+def FindLastColZeroIndex(sheet, row=0, col=0):
+  """
+  Find last populated column from starting point, returns 0 index value
+  """
+  content = sheet.cell_value(row, col)
+  colCount = 0
+
+  while content != "":
+    try:
+      content = sheet.cell_value(row, col + colCount)
+    except:
+      break
+    colCount = colCount + 1
+  
+  #zero indexing
+  colCount = colCount - 1
+  if colCount < 0:
+    colCount = 0
+
+  return colCount
 
 def GetStatusDetails(wb, row=0, col=0):
   """
@@ -47,3 +106,33 @@ def GetStatusDetails(wb, row=0, col=0):
     "renewalDate": renewalDate,
   }
   return acctStatus
+
+def Newb():
+  """
+  Create and return a new workbook object
+  """
+  workbook = xlwt.Workbook()
+  return workbook
+
+def News(workbook, name="New Sheet"):
+  worksheet = workbook.add_sheet(name)
+  return worksheet
+
+
+def Save(wb, NewWorkbookName="New Workbook.xlsx"):
+  """
+  Save the output in a workbook
+  """
+  NewWorkbookName = str(NewWorkbookName + ".xls")
+  wb.save(NewWorkbookName)
+  print("Saved: " + str(NewWorkbookName))
+
+
+def FindMissing(sheet1, sheet2):
+  """
+  Find missing data from sheet 1 in sheet 2
+  """
+  return
+
+
+
--- test.py
+++ test.py
@@ -7,21 +7,21 @@ col2 = 10
 row1 = 1
 row2 = 1
 
-rowCount = ec.FindLastPopulatedRow(wb1, row1)
-rowCount2 = ec.FindLastPopulatedRow(wb2, row2)
-
 sheet1 = ec.OpenSheet(wb1)
 sheet2 = ec.OpenSheet(wb2)
+
+rowCount = ec.FindLastRow(sheet1, row1)
+rowCount2 = ec.FindLastRow(sheet2, row2)
 
 for i in range(row1, rowCount):
   serial1 = ec.GetValue(sheet1, row1, col1)
   for x in range(row1, rowCount2):
     serial2 = ec.GetValue(sheet2, row2, col2)
     if serial1 == serial2:
-      
+      continue
     else:
       continue
-    write status, notes, renewalDate
+    #write status, notes, renewalDate
 
 print("rowCount " + str(rowCount))
 print("rowCount2 " + str(rowCount2))
